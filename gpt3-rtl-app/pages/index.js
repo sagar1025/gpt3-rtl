7e91d19bfe1e9f068bc6d6fd88c14bc97fff907e
Sagar Patel <sagar.patel@willowtreeapps.com>
2022-07-06 09:13:05 -0400

    add initial form to post

--- gpt3-rtl-app/pages/index.js
+++ gpt3-rtl-app/pages/index.js
@@ -1,13 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import  {Row, Container} from 'react-bootstrap';
+import  {Row, Container, Navbar, FloatingLabel, Form} from 'react-bootstrap';
 
 function Index() {
   return (
     <Container fluid>
       <Row>
-      <p>Next.js has ⭐️</p>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Container>
+            <Navbar.Brand href="#">Home</Navbar.Brand>
+          </Container>
+        </Navbar>
       </Row>
-      
+      <Row className="mt-4 mx-5">
+        <Container>
+          <Form>
+            <FloatingLabel
+                  controlId="componentArea"
+                  label="The Component"
+                  className="mb-3">
+                <Form.Control as="textarea" style={{ height: '500px' }}/>
+            </FloatingLabel>
+          </Form>
+        </Container>
+      </Row>
     </Container>
   )
 }
