444a3700d773257c692001573b580953dc493a40
Sagar Patel <sagar.patel@willowtreeapps.com>
2022-07-09 20:47:17 -0400

    pass in test cases

--- gpt3-rtl-app/pages/index.js
+++ gpt3-rtl-app/pages/index.js
@@ -6,13 +6,15 @@ import  {Row, Container, Navbar, Form, Button} from 'react-bootstrap';
 const Index = () => {
   const [hasResponse, setHasResponse] = useState('');
   const [componentCode, setComponentCode] = useState('');
+  const [testcases, setTestCases] = useState([]);
 
   const getResponse = async() => {
+    /* TO DO: USE the testCases state to be passed in to the API */
     const data = {
       model: "code-davinci-002",
-      prompt: `#Write Unit Tests for each of the following Test Cases given a React Component.\nTest Cases: \nBoth credentials is incorrect.\nemail is correct, but password is incorrect.\nemail is incorrect, but password is correct.\n\nReact Component:\n${componentCode}\n\nUnit Tests:\n`,
+      prompt: `#Write Unit Tests for each of the following Test Cases given a React Component.\nTest Cases: \nBoth credentials is incorrect.\nemail is correct, but password is incorrect.\nemail is incorrect, but password is correct.\n\nReact Component:\n${componentCode.trim()}\n\nUnit Tests:\n`,
       temperature: 0,
-      max_tokens: 2500,
+      max_tokens: 1500,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -51,14 +53,17 @@ const Index = () => {
       <Row className="mt-4 mx-5">
         <Container>
           <Form>
-            <label className="mb-3"> The Component Code</label>
+          <label className="mb-3"> Test Cases</label>
+            <Form.Control as="textarea" style={{ height: '150px' }} type="text" onChange={(e)=> setTestCases(testcases =>[...testcases, e.target.value])}/>
+            <label className="mb-3"> The Component</label>
             <Form.Control as="textarea" style={{ height: '500px' }} onChange={(e)=> setComponentCode(e.target.value)}/>
-            <Button variant="primary" size="lg" type="submit" onClick={handleSubmit}>Submit</Button>
+            <Button variant="primary" size="lg" type="submit" onClick={handleSubmit} className="mt-4">Submit</Button>
           </Form>
           {
             hasResponse && hasResponse !== '' 
             ?
             <div className="mt-5">
+              <h4>The unit test cases</h4>
               <pre>
                 {hasResponse}
               </pre>
